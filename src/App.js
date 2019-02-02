f5bf9720e874b73bc605f4e64024a0034c0ecd64
Zak <zakary.howard94@gmail.com>
2019-02-02 12:53:11 +0100

    commented out jsx code and uncommented html

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import './App.css';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, is this working?</h1>
-    //   </div>
-    // );
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'React createElement works!'))
+    return (
+      <div className="App">
+        <h1>Hi, is this working?</h1>
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'React createElement works!'))
   }
 }
 
