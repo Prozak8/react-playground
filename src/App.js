1f5423c756a0a5277fb42839b4aff3ecae76473f
Zak <zakary.howard94@gmail.com>
2019-02-02 15:45:45 +0100

    understanding click binding, arguments in different states

--- src/App.js
+++ src/App.js
@@ -9,22 +9,40 @@ class App extends Component {
       { name: 'Zak', age: 24 },
       { name: 'Jake', age: 9 },
       { name: 'Gabe', age: 9000 }
-    ]
+    ],
+    otherState: 'lorem ipsum'
   }
 
-  switchNameHandler = () => {
-    console.log('Was clicked');
+  switchNameHandler = ( newName ) => {
+    // console.log('Was clicked');
+    this.setState({
+      persons: [
+        { name: newName, age: 24 },
+        { name: 'Jake', age: 9 },
+        { name: 'Gabe', age: 10000 }
+      ]
+    })
   }
 
   render() {
     return (
       <div className="App">
         <h1>Hi, is this working?</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: MMA</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-
+        <button 
+          onClick={() => this.switchNameHandler('ZDH All Da Way')}>
+          Switch Name
+        </button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Jakey Boy!')}>
+          My Hobbies: MMA</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
       </div>
     ); 
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'React createElement works!'))
