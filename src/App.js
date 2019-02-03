7bc61944bc516fad4ef59b4937a0b3a85602f021
Zak <zakary.howard94@gmail.com>
2019-02-03 13:44:21 +0100

    experimenting with deleting specific person using index and event handler

--- src/App.js
+++ src/App.js
@@ -18,15 +18,10 @@ class App extends Component {
     showPersons: false
   }
 
-  switchNameHandler = ( newName ) => {
-    // console.log('Was clicked');
-    this.setState({
-      persons: [
-        { name: newName, age: 24 },
-        { name: 'Jake', age: 9 },
-        { name: 'Gabe', age: 10000 }
-      ]
-    })
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
   }
 
   nameChangedHandler = (event) => {
@@ -64,8 +59,9 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
-            return <Person 
+          {this.state.persons.map((person, index) => {
+            return <Person
+            click={() => this.deletePersonHandler(index)} 
               name={person.name} 
               age={person.age}/>
           })}
